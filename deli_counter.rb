a967fa7014f53ca69779346910216325d294f34e
Taylor Treece <tayloratreece@gmail.com>
2020-12-29 19:53:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
  if array.length == 0
    puts "The line is currently empty."
  else
-   status = "The line is currently: "
+   status = "The line is currently:"
    i = 1
    array.each do |index|
      status += " #{i}. #{index}"
