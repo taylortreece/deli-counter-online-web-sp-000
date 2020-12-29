ba5bc11ef6a17efbd454f2ae2d1d92d9e0267bcc
Taylor Treece <tayloratreece@gmail.com>
2020-12-29 19:56:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
  else
    status = "The line is currently:"
    array.each_with_index do |value, index|
-     status += " #{index.to_i + 1}. #{index}"
+     status += " #{index.to_i + 1}. #{value}"
     end
     puts status
    end
