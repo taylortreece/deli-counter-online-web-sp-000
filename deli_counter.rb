aba2d4f8854ef77a256402e2ca25367f74a61546
Taylor Treece <tayloratreece@gmail.com>
2020-12-29 20:13:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ def line(array)
 
 def take_a_number(name, array)
   array << name
-  puts "Welcome, #{array[-1]}. You are number #{array.length + 1} in line."
+  puts "Welcome, #{name}. You are number #{array.length + 1} in line."
 end
