ea8405abad93d85c2dca656493cbdb4b6583fca3
Taylor Treece <tayloratreece@gmail.com>
2020-12-29 20:22:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,8 @@ def now_serving(array)
   if array.length == 0
    puts "There is nobody waiting to be served!"
  else
-   customer = array.shift
+   customer = array[0]
    puts "Currently serving #{customer}."
+   array.shift
  end
 end
